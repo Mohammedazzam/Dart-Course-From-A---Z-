4b7c4ceac3055a6ed1491f4424efc723975b0331
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-02 00:54:49 +0300

    10(dart core global variable)

--- bin/main.dart
+++ bin/main.dart
@@ -1,30 +1,21 @@
- main(){
+ main() {
 
-//  String name = "foo";
-//         name = "xyz";
-//
-//  print(name);
-
-//سيطبع رسالة خطأ
-//   String name = "foo";
-//   String name = "xyz";
-//
+//   String name;
+//         name="fpp";
 //   print(name);
 
-
-//   bool name = true;
-//        name = false;
+//   String name;
 //   print(name);
 
- //سيطبع xyz
-//   List myinfo = ['foo','bar'];
-//   myinfo [0]= "xyz";
-//
-//   print(myinfo[0]);
+//    String name;
+//    name='123';
+//    print(name);
 
-   List myinfo = ['foo','bar'];
-   myinfo [0]= "xyz";
-   myinfo [1]= "test";
+//    String name;
+//      name='123';
+//    print(name);
 
-   print(myinfo[1]);
-}
+    int name;
+    name=123;
+    print(name);
+ }
