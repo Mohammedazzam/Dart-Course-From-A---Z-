0965678c5d993f68812bb8465973f63580133759
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-02 14:02:12 +0300

    16(lower and upper case String)

--- bin/main.dart
+++ bin/main.dart
@@ -1,22 +1,10 @@
 
 //main() {
 //  String name ="Foo";
-//  print(name.toString());
+//  print(name.toLowerCase());
 //}
-
-//main() {
-//  String name ="Foo";
-//  print(name.toString()[0]);
-//}
-
-
-//main() {
-//  String name ="Foo";
-//  print(name.toString().length);
-//}
-
 
 main() {
   String name ="Foo";
-  print(name.length);
+  print(name.toUpperCase());
 }
