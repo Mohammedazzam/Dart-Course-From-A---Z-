a767af84275190181c0aed00ad3cdf5bc23a870b
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-02 15:03:39 +0300

    24(forEach)

--- bin/main.dart
+++ bin/main.dart
@@ -1,31 +1,7 @@
 
-
-//  main() {
-//  List args =['foo','var','xyz'];
-//  print(args[0]);
-//  print(args[1]);
-//  print(args[2]);
-//}
-
-//main() {
-//  List args =['foo','var','xyz'];
-//  for(int index =0; index<args.length; index++){
-//    print(args[index]);
-//  }
-//}
-
-
-//main() {
-//  List args =['foo','var','xyz','action','test'];
-//  for(int index =0; index<args.length; index++){
-//    print(args[index]);
-//  }
-//}
-
-
 main() {
   List args =['foo','var','xyz','action','test'];
-  for(int index =0; index<args.length; index++){
-    print(index);
-  }
+  args.forEach((val){
+    print(val);
+    });
 }
