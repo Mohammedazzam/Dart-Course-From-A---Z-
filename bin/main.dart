e52bcdf1828c0e37fff17ad11646a6a789eb02e2
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-02 14:20:55 +0300

    20(dart math get max or min value)

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,14 @@
+import 'dart:math';
 
 //main() {
-//  int xyz(val){
-//    return val;
-//  }
-//  print(xyz(1));
-//}
-
-//main() {
-//  int xyz(val){
-//    return val;
-//  }
-//  print(xyz(100));
-//}
-
-//main() {
-//  int xyz(val){
-//    return val * 3;
-//  }
-//  print(xyz(100));
-//}
-
-//main() {
-//  int xyz(val1,val2){
-//    return val1 * val2;
-//  }
-//  print(xyz(50,40));
+//  int value1 = 100;
+//  int value2 = 200;
+//  print(max(value1, value2));
 //}
 
 
 main() {
-  int xyz(val1,val2){
-    return val1 + val2;
-  }
-  print(xyz(50,40));
+  int value1 = 100;
+  int value2 = 200;
+  print(min(value1, value2));
 }
