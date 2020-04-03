40081042eca9c5113e2ec20f43465b9b985af202
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-03 20:55:08 +0300

    25(List type key and value data)

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,65 @@
+//main() {
+//  List args = [{
+//    "name":"Foo"
+//  }];
+//  print(args);
+//}
+
+
+//main() {
+//  List args = [{
+//    "name":"Foo"
+//  }];
+//  print(args[0]);
+//}
+
+//main() {
+//  List args = [{
+//    "name":"Foo"
+//  }];
+//  print(args[0]["name"]); //هيك جبت ال kye الي بجيب منها ال value
+//}
+
+
+
+//main() {
+//  List args = [{
+//    "name":"Foo",
+//    "age" :20,
+//    "isonline":false
+//  }];
+//  print(args);
+//}
+
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  }];
+//  print(args[0]["isonline"]);
+//}
+
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  }];
+//  args.forEach((key){
+//    print(key["name"]);
+//  });
+//}
 
 main() {
-  List args =['foo','var','xyz','action','test'];
-  args.forEach((val){
-    print(val);
-    });
+  List args = [{
+    "name": "Foo",
+    "age": 20,
+    "isonline": false
+  }];
+  for(int i = 0; i<args.length; i++){
+    print(args[i]['age']);
+  }
 }
