300872a166c8952ffd3d8f13153d286d61bab9a8
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-04 00:27:55 +0300

    25(List type key and value data تابع)

--- bin/main.dart
+++ bin/main.dart
@@ -53,13 +53,131 @@
 //  });
 //}
 
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  }];
+//  for(int i = 0; i<args.length; i++){
+//    print(args[i]['age']);
+//  }
+//}
+
+
+import 'package:test/test.dart';
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  },
+//
+//    {
+//      "name": "bar",
+//      "age": 30,
+//      "isonline": true
+//    }
+//
+//  ];
+//  for(int i = 0; i<args.length; i++){
+//    print(args[i]['age']);
+//  }
+//}
+
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  },
+//
+//    {
+//      "name": "bar",
+//      "age": 30,
+//      "isonline": true
+//    }
+//
+//  ];
+//  for(int i = 0; i<args.length; i++){
+//    print(args[i]['name']);
+//  }
+//}
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  },
+//
+//    {
+//      "name": "bar",
+//      "age": 30,
+//      "isonline": true
+//    }
+//
+//  ];
+//  for(int i = 0; i<args.length; i++){
+//print("${args[i]['name']} ${args[i]['age']}");
+//}
+//}
+
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  },
+//
+//    {
+//      "name": "bar",
+//      "age": 30,
+//      "isonline": true
+//    }
+//
+//  ];
+//  for(int i = 0; i<args.length; i++){
+//    print("name:${args[i]['name']} age:${args[i]['age']}  online:${args[i]['isonline']}");
+//  }
+//}
+
+
+//main() {
+//  List args = [{
+//    "name": "Foo",
+//    "age": 20,
+//    "isonline": false
+//  },
+//
+//    {
+//      "name": "bar",
+//      "age": 30,
+//      "isonline": true
+//    }
+//
+//  ];
+//  args.forEach((key) {
+//    print("name:${key['name']} age:${key['age']}  online:${key['isonline']}");
+//  });
+//}
+
 main() {
   List args = [{
     "name": "Foo",
     "age": 20,
     "isonline": false
-  }];
-  for(int i = 0; i<args.length; i++){
-    print(args[i]['age']);
-  }
+  },
+
+    {
+      "name": "bar",
+      "age": 30,
+      "isonline": true
+    }
+
+  ];
+  args.forEach((key)=>print("name:${key['name']} age:${key['age']}  online:${key['isonline']}"));
 }
