e11950fc0b6136722fd6dacf9938765a6f285830
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-05 22:59:50 +0300

    29(if Conditions if & elseif & else)

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,39 @@
 
 
-import 'package:App/App.dart' as func;
+import 'package:App/App.dart' as dart;
 
-main(){
-  print(func.names);
+//main(){
+//  int year = 2019;
+//  if(year>0){
+//    print("yes is max");
+//  }
+//  }
+
+
+//main(){
+//  int year = 0;
+//  if(year>0){
+//    print("yes is max");
+//  }
+//}
+
+//main(){
+//  int year = 0;
+//  if(year>0){
+//    print("yes is max");
+//  }else{
+//    print("no is not max");
+//  }
+//}
+
+
+main() {
+  int year = 0;
+  if (year > 0) {
+    print("yes is max");
+  } else if (year == 0) {
+    print("is equle");
+  } else {
+    print("no is not max");
+  }
 }
--- test/App_test.dart
+++ test/App_test.dart
@@ -2,7 +2,5 @@ import 'package:App/App.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
 }
