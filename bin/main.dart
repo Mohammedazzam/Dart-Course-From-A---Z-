0b60c750b96a30e492dec6e39a3b5248389591d8
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-05 23:33:02 +0300

    30( OR || and && symbol)

--- bin/main.dart
+++ bin/main.dart
@@ -2,34 +2,25 @@
 
 import 'package:App/App.dart' as dart;
 
-//main(){
-//  int year = 2019;
-//  if(year>0){
-//    print("yes is max");
-//  }
-//  }
-
-
-//main(){
+//
+//main() {
 //  int year = 0;
-//  if(year>0){
+//  int month =4;
+//  if (year > 0 || month ==4) {
 //    print("yes is max");
-//  }
-//}
-
-//main(){
-//  int year = 0;
-//  if(year>0){
-//    print("yes is max");
-//  }else{
+//  } else if (year == 0) {
+//    print("is equle");
+//  } else {
 //    print("no is not max");
 //  }
 //}
 
 
+
 main() {
   int year = 0;
-  if (year > 0) {
+  int month =4;
+  if (year > 0 && month ==4) {
     print("yes is max");
   } else if (year == 0) {
     print("is equle");
