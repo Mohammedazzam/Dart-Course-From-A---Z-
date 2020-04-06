9bed816216d0c94fbb03897094331cd7a9e8f972
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-06 15:57:56 +0300

    38(add method to class and use it as object)

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,45 @@
+
+
+//main(){
+//  var fooClass = new FooBar();
+//  print(fooClass.xyz());
+////  fooClass.bar();
+//}
+//class FooBar{
+//  String xyz(){
+//    return "This Is String";
+//  }
+//  void bar(){
+//    print("i am Bar Method");
+//  }
+//}
+
+
+//main(){
+//  var fooClass = new FooBar();
+//  print(fooClass.xyz());
+////  fooClass.bar();
+//}
+//class FooBar{
+// String xyz(){
+//    return 23;
+//  }
+//  void bar(){
+//    print("i am Bar Method");
+//  }
+//}
+
+
 main(){
   var fooClass = new FooBar();
-  print(fooClass);
-}
-class FooBar{
+  print(fooClass.name);
 
 }
+  class FooBar{
+    var name ="hey i am Foo value";
+
+}
+
+
+
+
