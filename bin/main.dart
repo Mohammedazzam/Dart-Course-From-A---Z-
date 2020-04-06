c0690db19d6b38bdfc0f3e1f24387d6ffbb5ccb4
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-06 15:13:45 +0300

    31(do while)

--- bin/main.dart
+++ bin/main.dart
@@ -2,63 +2,30 @@
 
 import 'package:App/App.dart' as dart;
 
-
 //main() {
-//  int year = 2019;
-//  year == 0 ? print("is True") :print("is False");
-//}
-
-//main() {
-//  int year = 2019;
-//  year > 0 ?
-//      year ==2019 ? print("is True") : print("is False")
-//            :
-//  print("is False");
-//}
-
-
-
-//main() {
-//  int year = 2019;
-//  year > 0 ?
-//  year !=2019 ? print("is True") : print("is False")
-//      :
-//  print("is False");
-//}
-
-
-//main() {
-//  int year = 2019;
-//  if(year != 0){
-//    print("is true");
-//  }
-//}
-
-
-//main() {
-//  int year = 2019;
-//  if(year <= 0){
-//    print("is true");
-//  }else{
-//    print("is no");
-//  }
-//}
-
-//main() {
-//  int year = 2019;
-//  if(year >= 0){
-//    print("is true");
-//  }else{
-//    print("is no");
-//  }
+//  List names =["foo","bar","xyz"];
+//
+//  int index = names.length;
+//
+//  print(index);
+//
 //}
 
 
 main() {
-  int year = 2019;
-  if(year == 0 || year ==0){
-    print("is true");
-  }else{
-    print("is no");
-  }
+  List names =["foo","bar","xyz"];
+  int index = names.length;
+  do {
+    index --;
+    print(index);
+  }while(index> 0);
 }
+
+//main() {
+//  List names =["foo","bar","xyz"];
+//  int index = names.length;
+//  do {
+//    index --;
+//    print(names[index]);
+//  }while(index> 0);
+//  }
