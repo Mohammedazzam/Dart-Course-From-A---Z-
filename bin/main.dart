2cc19a82c817da4076efb60d3b60e98ae3bb4f31
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-06 15:17:14 +0300

    34(Dead code)

--- bin/main.dart
+++ bin/main.dart
@@ -1,31 +1,16 @@
 
 
-import 'package:App/App.dart' as dart;
-
-//main() {
-//  List names =["foo","bar","xyz"];
-//
-//  int index = names.length;
-//
-//  print(index);
-//
+//main(){
+//  foo(){
+//    return 1;
+//  }
+//  print(foo());
 //}
 
-
-main() {
-  List names =["foo","bar","xyz"];
-  int index = names.length;
-  do {
-    index --;
-    print(index);
-  }while(index> 0);
+main(){
+  foo(){
+    return 1;
+    return 2;
+  }
+  print(foo());
 }
-
-//main() {
-//  List names =["foo","bar","xyz"];
-//  int index = names.length;
-//  do {
-//    index --;
-//    print(names[index]);
-//  }while(index> 0);
-//  }
