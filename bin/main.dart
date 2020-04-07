b1b21df6e12081c857b4f9095f7512348a489bbe
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-08 00:42:13 +0300

    55(Setter Object)

--- bin/main.dart
+++ bin/main.dart
@@ -1,104 +1,95 @@
 
-
 //main(){
 //  var fooclass = new FooBar();
+//  fooclass.names = "xyz";
 //  print(fooclass.name);
-//}
-//
-//
-//class FooBar{
-//  String name = "Test";
-//}
-
-
-
-//main(){
-//  var fooclass = new FooBar();
-//  fooclass.name = "foo"; //هذه هي عملية ال setter
-//  print(fooclass.name);
-//}
-//
-//
-//class FooBar{
-//  String name = "Test";
-//}
-
-//سيطبع null لأنني لم أحدد قيمة
-//main(){
-//  var fooclass = new FooBar();
-//  print(fooclass.names);
 //}
 //
 //class FooBar{
 //  String name;
+//
 //  String get names { //هنا بمعنى انني سأنشئ object
-//    return name;
+//  }
+//  void set names(String value){
+//    this.name = value;
 //  }
 //}
 
 
 //main(){
 //  var fooclass = new FooBar();
-//  print(fooclass.names);
-//}
-//
-//class FooBar{
-//  String name ="test";
-//  String get names { //هنا بمعنى انني سأنشئ object
-//    return name;
-//  }
-//}
-
-
-//طريقة أخرى لعمل set
-//main(){
-//  var fooclass = new FooBar("Data Set");
-//  print(fooclass.names);
-//}
-//
-//class FooBar{
-//  String name ="test";
-//  FooBar(this.name);
-//  String get names { //هنا بمعنى انني سأنشئ object
-//    return name;
-//  }
-//}
-
-
-//طريقة ثالثة
-//main(){
-//  var fooclass = new FooBar(1);
-//  print(fooclass.names);
+//  fooclass.names = 1;
+//  print(fooclass.name);
 //}
 //
 //class FooBar{
 //  int name;
-//  FooBar(this.name);
 //
 //  String get names { //هنا بمعنى انني سأنشئ object
-//    if(this.name == 1 ){
-//      return "Foo";
-//    }else if(this.name == 2){
-//      return "bar";
+//  }
+//  void set names(int value){
+//    this.name = value;
+//  }
+//}
+
+
+//main(){
+//  var fooclass = new FooBar();
+//  fooclass.names = 1000;
+//  print(fooclass.name);
+//}
+//
+//class FooBar{
+//  int name;
+//
+//  String get names { //هنا بمعنى انني سأنشئ object
+//  }
+//  void set names(int value){
+//    if(value == 1000){
+//      this.name = 2000;
+//
+//    }
+//  }
+//}
+
+//main(){
+//  var fooclass = new FooBar();
+//  fooclass.names = 0;
+//  print(fooclass.name);
+//}
+//
+//class FooBar{
+//  int name;
+//
+//  String get names { //هنا بمعنى انني سأنشئ object
+//  }
+//  void set names(int value){
+//    if(value == 1000){
+//      this.name = 2000;
+//    }else{
+//      this.name = 0;
 //    }
 //  }
 //}
 
 
 main(){
-  var fooclass = new FooBar(2);
+  var fooclass = new FooBar();
+  fooclass.names = 0; //Setter
   print(fooclass.names);
 }
 
 class FooBar{
   int name;
-  FooBar(this.name);
 
-  String get names { //هنا بمعنى انني سأنشئ object
-    if(this.name == 1 ){
-      return "Foo";
-    }else if(this.name == 2){
-      return "bar";
+  int get names { //هنا بمعنى انني سأنشئ object
+    return name = name;
+  }
+  void set names(int value){
+    if(value == 1000){ //لو 1000 راح ترجع 1000 ولو 0 راح ترجع 0 حسب القيمة الي فوق
+      this.name = 2000;
+    }else{
+      this.name = 0;
     }
   }
 }
