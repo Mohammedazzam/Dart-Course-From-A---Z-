a3b4c98ef8923b8a16177af5e275731c2d640e64
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-07 15:45:39 +0300

    53(implement multiple Interfaces)

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,98 @@
 
-
 //main(){
 //  var fooclass = new FooBar();
-//  print(fooclass.name);
+//  print(fooclass.dogname());
 //}
 //class MainClass{
 //  String name;
 //}
-//class FooBar implements MainClass{
-//  String name = "foo";
+//
+//class Dog{
+// String dogname(){
+//
+// }
+//}
+//
+//class FooBar implements MainClass , Dog{
+//  String name = "bar";
+//  int age = 20;
+//  String dogname(){
+//    return "rex";
+//  }
+//
 //
 //}
 
+
+//main(){
+//  var fooclass = new FooBar();
+//  print(fooclass.dogname());
+//}
+//class MainClass{
+//  String name;
+//}
+//
+//class Dog{
+//  String dogname(){
+//
+//  }
+//}
+//
+//class FooBar implements MainClass , Dog{
+//  String name = "bar";
+//  int age = 20;
+//  String dogname(){
+//    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
+//  }
+//
+//
+//}
+
+
+
+//main(){
+//  var fooclass = new FooBar();
+//  print(fooclass.dogname());
+//}
+//class MainClass{
+//  String name;
+//}
+//
+//class Dog{
+//  void dogname(){} //هيك أنا حر بتحديد القيمة بعكس لو حكتلو String dogname
+//}
+//
+//class FooBar implements MainClass , Dog{
+//  String name = "bar";
+//  int age = 20;
+//  int dogname(){
+//    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
+//  }
+//
+//
+//}
+
+
 main(){
   var fooclass = new FooBar();
-  print(fooclass.name);
+  print(fooclass.names);
 }
 class MainClass{
   String name;
 }
-class FooBar implements MainClass{
+
+class Dog{
+  var names;
+  void dogname(){} //هيك أنا حر بتحديد القيمة بعكس لو حكتلو String dogname
+}
+
+class FooBar implements MainClass , Dog{
+  var names = ["foo","bar","xyz"];
   String name = "bar";
   int age = 20;
+  int dogname(){
+    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
+  }
+
 
 }
