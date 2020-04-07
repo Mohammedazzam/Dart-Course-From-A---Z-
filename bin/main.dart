ed70b2153202749bad5b3b2e901b388928f75fcc
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-07 15:59:41 +0300

    54(Getter Object)

--- bin/main.dart
+++ bin/main.dart
@@ -1,98 +1,104 @@
 
-//main(){
-//  var fooclass = new FooBar();
-//  print(fooclass.dogname());
-//}
-//class MainClass{
-//  String name;
-//}
-//
-//class Dog{
-// String dogname(){
-//
-// }
-//}
-//
-//class FooBar implements MainClass , Dog{
-//  String name = "bar";
-//  int age = 20;
-//  String dogname(){
-//    return "rex";
-//  }
-//
-//
-//}
-
 
 //main(){
 //  var fooclass = new FooBar();
-//  print(fooclass.dogname());
-//}
-//class MainClass{
-//  String name;
+//  print(fooclass.name);
 //}
 //
-//class Dog{
-//  String dogname(){
 //
-//  }
-//}
-//
-//class FooBar implements MainClass , Dog{
-//  String name = "bar";
-//  int age = 20;
-//  String dogname(){
-//    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
-//  }
-//
-//
+//class FooBar{
+//  String name = "Test";
 //}
 
 
 
 //main(){
 //  var fooclass = new FooBar();
-//  print(fooclass.dogname());
+//  fooclass.name = "foo"; //هذه هي عملية ال setter
+//  print(fooclass.name);
 //}
-//class MainClass{
+//
+//
+//class FooBar{
+//  String name = "Test";
+//}
+
+//سيطبع null لأنني لم أحدد قيمة
+//main(){
+//  var fooclass = new FooBar();
+//  print(fooclass.names);
+//}
+//
+//class FooBar{
 //  String name;
-//}
-//
-//class Dog{
-//  void dogname(){} //هيك أنا حر بتحديد القيمة بعكس لو حكتلو String dogname
-//}
-//
-//class FooBar implements MainClass , Dog{
-//  String name = "bar";
-//  int age = 20;
-//  int dogname(){
-//    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
+//  String get names { //هنا بمعنى انني سأنشئ object
+//    return name;
 //  }
+//}
+
+
+//main(){
+//  var fooclass = new FooBar();
+//  print(fooclass.names);
+//}
 //
+//class FooBar{
+//  String name ="test";
+//  String get names { //هنا بمعنى انني سأنشئ object
+//    return name;
+//  }
+//}
+
+
+//طريقة أخرى لعمل set
+//main(){
+//  var fooclass = new FooBar("Data Set");
+//  print(fooclass.names);
+//}
 //
+//class FooBar{
+//  String name ="test";
+//  FooBar(this.name);
+//  String get names { //هنا بمعنى انني سأنشئ object
+//    return name;
+//  }
+//}
+
+
+//طريقة ثالثة
+//main(){
+//  var fooclass = new FooBar(1);
+//  print(fooclass.names);
+//}
+//
+//class FooBar{
+//  int name;
+//  FooBar(this.name);
+//
+//  String get names { //هنا بمعنى انني سأنشئ object
+//    if(this.name == 1 ){
+//      return "Foo";
+//    }else if(this.name == 2){
+//      return "bar";
+//    }
+//  }
 //}
 
 
 main(){
-  var fooclass = new FooBar();
+  var fooclass = new FooBar(2);
   print(fooclass.names);
 }
-class MainClass{
-  String name;
-}
 
-class Dog{
-  var names;
-  void dogname(){} //هيك أنا حر بتحديد القيمة بعكس لو حكتلو String dogname
-}
+class FooBar{
+  int name;
+  FooBar(this.name);
 
-class FooBar implements MainClass , Dog{
-  var names = ["foo","bar","xyz"];
-  String name = "bar";
-  int age = 20;
-  int dogname(){
-    return 1; //سيظهر خطأ لأنني أنا محدد من فوق انو النوع string
+  String get names { //هنا بمعنى انني سأنشئ object
+    if(this.name == 1 ){
+      return "Foo";
+    }else if(this.name == 2){
+      return "bar";
+    }
   }
-
-
 }
