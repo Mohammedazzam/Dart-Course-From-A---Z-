69f5d06cc25c12441787a1715ddcf8586c826b1f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-07 14:47:18 +0300

    47(more explanation about super Class

--- bin/main.dart
+++ bin/main.dart
@@ -7,14 +7,9 @@ main(){
 }
 class FooBar {
   String name = "foo";
-  int age = 20;
-  List names =  ['foo','xyz', 'bar'];
   FooBar(this.name);
-  double price(){
-    return 300.5;
   }
 
-}
 
 class Dog extends FooBar{
   Dog(String name) : super(name);
