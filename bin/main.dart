6ef9c47150d9b40a87a29a5e6a278e110bbbde69
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-07 15:17:57 +0300

    51(use SuperClass and override)

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,4 @@
 
-
 //main(){
 //  var foobar = new FooBar();
 //  print(foobar.xyz());
@@ -7,55 +6,92 @@
 //abstract class Rules{
 //  String name = "foo";
 //  int xyz(){
-//    return 1000;
+//    return 0;
 //  }
 //
 //}
 //class FooBar extends Rules{
 //  int age = 20;
 //  String name = "bar";
+//}
 //
+//class Cat extends FooBar{
+//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
+//  int xyz(){
+//    return super.xyz();
+//  }
 //}
 
 
-//لو بدي اعمل override
+
+//main(){
+//  var foobar = new FooBar();
+//  print(foobar.xyz(1));
+//}
+//abstract class Rules{
+//  String name = "foo";
+//  int xyz(int val){
+//    return 0;
+//  }
+//
+//}
+//class FooBar extends Rules{
+//  int age = 20;
+//  String name = "bar";
+//}
+//
+//class Cat extends FooBar{
+//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
+//  int xyz(supval){
+//    return super.xyz(supval);
+//  }
+//}
+
+
+//main(){
+//  var foobar = new FooBar();
+//  print(foobar.xyz("")); //لازم تكون القيمة من نوع int كما قمت بتحديدها مسبقاً
+//}
+//abstract class Rules{
+//  String name = "foo";
+//  int xyz(int val){
+//    return 0;
+//  }
+//
+//}
+//class FooBar extends Rules{
+//  int age = 20;
+//  String name = "bar";
+//}
+//
+//class Cat extends FooBar{
+//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
+//  int xyz(supval){
+//    return super.xyz(supval);
+//  }
+//}
+
+
+
 main(){
   var foobar = new FooBar();
-  print(foobar.xyz());
+  print(foobar.xyz(10000)); //لازم تكون القيمة من نوع int كما قمت بتحديدها مسبقاً
 }
 abstract class Rules{
   String name = "foo";
-  int xyz(){
-    return 1000;
+  int xyz(int val){
+    return val;
   }
 
 }
 class FooBar extends Rules{
   int age = 20;
   String name = "bar";
-  xyz() {
-    return 3000;
-  }
 }
 
-
-////لو بدي اعمل override
-//main(){
-//  var foobar = new FooBar();
-//  print(foobar.xyz());
-//}
-//abstract class Rules{
-//  String name = "foo";
-//  int xyz(){
-//    return 1000;
-//  }
-//
-//}
-//class FooBar extends Rules{
-//  int age = 20;
-//  String name = "bar";
-//  int xyz() {
-//    return 3000;
-//  }
-//
-//}
+class Cat extends FooBar{
+  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
+  int xyz(supval){
+    return super.xyz(supval);
+  }
+}
