cef39f2485c076385385bdd9b2f4da1321016488
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-08 01:16:12 +0300

    60(dart core Map   Functions remove & clear & addAll and Extract data)

--- bin/main.dart
+++ bin/main.dart
@@ -1,30 +1,93 @@
 
 
 //main(){
-//  var names = new Map();
-//  names['name1'] = 'Foo';
-//  names['name2'] = 'bar';
-//  names['name3'] = 'xyz';
-//
-//  print(names);
+//  var names = {'name1':'foo','name2':'bar','name3':'xyz'};
+//  names.forEach((key,value){
+//    print(key);
+//  });
 //}
 
 
 //main(){
-//  var names = new Map();
-//  names['name1'] = 'Foo';
-//  names['name2'] = 'bar';
-//  names['name3'] = 'xyz';
-//
-//  print(names.values);
+//  var names = {'name1':'foo','name2':'bar','name3':'xyz'};
+//  names.forEach((key,value){
+//    print(value);
+//  });
 //}
 
 
-main(){
+
+//main(){
+//  var names = new Map();
+//  names['name1'] = 'foo';
+//  names['name2'] = 'bar';
+//  names['name3'] = 'xyz';
+//  names.forEach((key,value){
+//    print(value); //سيطبع نفس القيم
+//  });
+//}
+
+//main() {
+//  var names = new Map();
+//  names['name1'] = 'foo';
+//  names['name2'] = 'bar';
+//  names['name3'] = 'xyz';
+//  names.forEach((key, value) {
+//    print('$key = $value'); //سيطبع نفس القيم
+//  });
+//}
+
+
+//main() {
+//  var names = new Map();
+//  names['name1'] = 'foo';
+//  names['name2'] = 'bar';
+//  names['name3'] = 'xyz';
+//
+//  names.remove('name3');//لحذف قيمة
+//
+//  names.forEach((key, value) {
+//    print('$key = $value');
+//  });
+//}
+
+
+//main() {
+//  var names = new Map();
+//  names['name1'] = 'foo';
+//  names['name2'] = 'bar';
+//  names['name3'] = 'xyz';
+//
+//  names.clear();//تحذف كل ال map
+//
+//  names.forEach((key, value) {
+//    print('$key = $value'); //ماراح يطبع ششيئ
+//  });
+//}
+
+
+//main() {
+//  var names = new Map();
+//  names['name1'] = 'foo';
+//  names['name2'] = 'bar';
+//  names['name3'] = 'xyz';
+//
+//  names.clear();//تحذف كل ال map
+//  print(names.length);
+//  names.forEach((key, value) {
+//    print('$key = $value'); //ماراح يطبع ششيئ
+//  });
+//}
+
+
+main() {
   var names = new Map();
-  names['name1'] = 'Foo';
+  names['name1'] = 'foo';
   names['name2'] = 'bar';
   names['name3'] = 'xyz';
 
-  print(names.values.toList()); //حولتها ل list يعني كإنني حولتها ل array
+  names.addAll({'age':20,'salary':200});//هذه تضيف map جديدة
+  names.forEach((key, value) {
+    print('$key = $value'); //ماراح يطبع ششيئ
+  });
 }
