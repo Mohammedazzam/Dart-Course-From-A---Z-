5c790f8769e3e23b35e1aa30186c2923220e327f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-07 15:31:08 +0300

    52(what is the Implementing)

--- bin/main.dart
+++ bin/main.dart
@@ -1,97 +1,26 @@
 
-//main(){
-//  var foobar = new FooBar();
-//  print(foobar.xyz());
-//}
-//abstract class Rules{
-//  String name = "foo";
-//  int xyz(){
-//    return 0;
-//  }
-//
-//}
-//class FooBar extends Rules{
-//  int age = 20;
-//  String name = "bar";
-//}
-//
-//class Cat extends FooBar{
-//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
-//  int xyz(){
-//    return super.xyz();
-//  }
-//}
-
-
 
 //main(){
-//  var foobar = new FooBar();
-//  print(foobar.xyz(1));
+//  var fooclass = new FooBar();
+//  print(fooclass.name);
 //}
-//abstract class Rules{
+//class MainClass{
+//  String name;
+//}
+//class FooBar implements MainClass{
 //  String name = "foo";
-//  int xyz(int val){
-//    return 0;
-//  }
 //
 //}
-//class FooBar extends Rules{
-//  int age = 20;
-//  String name = "bar";
-//}
-//
-//class Cat extends FooBar{
-//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
-//  int xyz(supval){
-//    return super.xyz(supval);
-//  }
-//}
-
-
-//main(){
-//  var foobar = new FooBar();
-//  print(foobar.xyz("")); //لازم تكون القيمة من نوع int كما قمت بتحديدها مسبقاً
-//}
-//abstract class Rules{
-//  String name = "foo";
-//  int xyz(int val){
-//    return 0;
-//  }
-//
-//}
-//class FooBar extends Rules{
-//  int age = 20;
-//  String name = "bar";
-//}
-//
-//class Cat extends FooBar{
-//  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
-//  int xyz(supval){
-//    return super.xyz(supval);
-//  }
-//}
-
-
 
 main(){
-  var foobar = new FooBar();
-  print(foobar.xyz(10000)); //لازم تكون القيمة من نوع int كما قمت بتحديدها مسبقاً
+  var fooclass = new FooBar();
+  print(fooclass.name);
 }
-abstract class Rules{
-  String name = "foo";
-  int xyz(int val){
-    return val;
-  }
-
+class MainClass{
+  String name;
 }
-class FooBar extends Rules{
-  int age = 20;
+class FooBar implements MainClass{
   String name = "bar";
-}
+  int age = 20;
 
-class Cat extends FooBar{
-  @override //هذه بمعنى اكتب على القيمة الي فوق وهات القيمة الخاصة بها
-  int xyz(supval){
-    return super.xyz(supval);
-  }
 }
