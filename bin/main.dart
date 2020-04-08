8fa349f96d74b0f99a8d7505cf9e91558ede80ae
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 01:40:19 +0300

    72(Enumeration)

--- bin/main.dart
+++ bin/main.dart
@@ -1,29 +1,41 @@
-
-
-//main(){
-//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
-//  DateTime clock = DateTime.now();
-//  print(clock);
+//enum data{
+//  data1,
+//  data2,
+//  data3
 //}
-
 //main(){
-//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
-//  DateTime clock = DateTime.now().add(du); //هيك أنا ضفت للوقت الحالي الوقت الي فوق
-//  print(clock);
+//   print(data);
 //}
 
 
-
+//enum data{
+//  data1,
+//  data2,
+//  data3
+//}
 //main(){
-//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
-//  DateTime clock = DateTime.now().toLocal(); //الوقت حسب المنطقة الزمنية
-//  print(clock);
+//  print(data.values);
 //}
 
 
+//enum data{
+//  data1,
+//  data2,
+//  data3
+//}
+//main(){
+//  print(data.data1);
+//}
 
+
+enum data{
+  data1,
+  data2,
+  data3
+}
 main(){
-  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
-  DateTime clock = DateTime.utc(1980);
-  print(clock);
+  var test = data.data1;
+  if(test == data.data1){
+    print('hi enum Data');
+  }
 }
