ea170b21ba44418e2e6e6221f23aec5a07c8dc23
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 01:34:37 +0300

    71(use the Duration class in DateTime class)

--- bin/main.dart
+++ bin/main.dart
@@ -1,22 +1,29 @@
-//main(){
-//  Duration du = Duration(minutes: 5);
-//  print(du);
-//}
 
 
 //main(){
-//  Duration du = Duration(minutes: 5,hours: 2);
-//  print(du);
+//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
+//  DateTime clock = DateTime.now();
+//  print(clock);
 //}
+
+//main(){
+//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
+//  DateTime clock = DateTime.now().add(du); //هيك أنا ضفت للوقت الحالي الوقت الي فوق
+//  print(clock);
+//}
+
 
 
 //main(){
-//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50);
-//  print(du);
+//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
+//  DateTime clock = DateTime.now().toLocal(); //الوقت حسب المنطقة الزمنية
+//  print(clock);
 //}
+
 
 
 main(){
   Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
-  print(du);
+  DateTime clock = DateTime.utc(1980);
+  print(clock);
 }
