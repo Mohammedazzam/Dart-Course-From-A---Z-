afdc631528599ba31e98fe0c77db66f3a6042dd0
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 01:17:28 +0300

    70(what is the Duration Class)

--- bin/main.dart
+++ bin/main.dart
@@ -1,107 +1,22 @@
 //main(){
-//  List name = [1,2,3];
-//
-//  print(name);
+//  Duration du = Duration(minutes: 5);
+//  print(du);
 //}
 
 
 //main(){
-//  List <Dog> name = <Dog>[];
-//  name.add(Dog('foo', 12));
-//  print(name);
-//
-//}
-//class Dog{
-//  String name;
-//  int age;
-//  Dog(this.name, this.age);
+//  Duration du = Duration(minutes: 5,hours: 2);
+//  print(du);
 //}
 
 
 //main(){
-//  List <Dog> name = <Dog>[]; //ال Dog الأولى قمت بتحيديد النوع ,والثانية قمت بإسناد القيمة
-//  name.add(Dog('foo', 12));//هيك قمت بتعيين القيم
-//  print(name);
-//
+//  Duration du = Duration(minutes: 5,hours: 2,seconds: 50);
+//  print(du);
 //}
-//class Dog{
-//  String name;
-//  int age;
-//  Dog(this.name, this.age);
-//  //لإظهار القيم من ال object الذي قمت بتعيينه وهنالك أكثر من طريقة منها map
-//
-//}
-
-
-//main(){
-//  List <Dog> name = <Dog>[]; //ال Dog الأولى قمت بتحيديد النوع ,والثانية قمت بإسناد القيمة
-//  name.add(Dog('foo', 12));//هيك قمت بتعيين القيم
-//  name.add(Dog('bar', 30));
-//  //لإظهار القيم من ال object الذي قمت بتعيينه وهنالك أكثر من طريقة منها map
-//  name.map((Dog data){
-//    print(data.name);
-//  }).toList(); //هذا الأمر خاص بتحويل ال map إلى List
-//
-//}
-//class Dog{
-//  String name;
-//  int age;
-//  Dog(this.name, this.age);
-//
-//}
-
-
-//main(){
-//  List <Dog> name = <Dog>[]; //ال Dog الأولى قمت بتحيديد النوع ,والثانية قمت بإسناد القيمة
-//  name.add(Dog('foo', 12));//هيك قمت بتعيين القيم
-//  name.add(Dog('bar', 30));
-//  //لإظهار القيم من ال object الذي قمت بتعيينه وهنالك أكثر من طريقة منها map
-//  name.map((Dog data){
-//    print(data.age);
-//  }).toList(); //هذا الأمر خاص بتحويل ال map إلى List
-//
-//}
-//class Dog{
-//  String name;
-//  int age;
-//  Dog(this.name, this.age);
-//
-//}
-
-//main(){
-//  List <Dog> name = <Dog>[]; //ال Dog الأولى قمت بتحيديد النوع ,والثانية قمت بإسناد القيمة
-//  name.add(Dog('foo', 12,false));//هيك قمت بتعيين القيم
-//  name.add(Dog('bar', 30,true));
-//  //لإظهار القيم من ال object الذي قمت بتعيينه وهنالك أكثر من طريقة منها map
-//  name.map((Dog data){
-//    print(data.xyz);
-//  }).toList(); //هذا الأمر خاص بتحويل ال map إلى List
-//
-//}
-//class Dog{
-//  String name;
-//  int age;
-//  bool xyz;
-//  Dog(this.name, this.age, this.xyz);
-//
-//}
-
 
 
 main(){
-  List <Dog> name = <Dog>[]; //ال Dog الأولى قمت بتحيديد النوع ,والثانية قمت بإسناد القيمة
-  name.add(Dog('foo', 12,false));//هيك قمت بتعيين القيم
-  name.add(Dog('bar', 30,true));
-  //لإظهار القيم من ال object الذي قمت بتعيينه وهنالك أكثر من طريقة منها map
-  name.map((Dog data){
-    print('${data.xyz} ${data.name} ${data.age}');
-  }).toList(); //هذا الأمر خاص بتحويل ال map إلى List
-
-}
-class Dog{
-  String name;
-  int age;
-  bool xyz;
-  Dog(this.name, this.age, this.xyz);
-
+  Duration du = Duration(minutes: 5,hours: 2,seconds: 50,microseconds: 999999);
+  print(du);
 }
