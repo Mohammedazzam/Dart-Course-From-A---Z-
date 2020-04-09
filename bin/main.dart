a9d77b67fa446702464441aed04bb7a3e2a5ee5e
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 17:46:47 +0300

    80(dart collection SplayTreeMap)

--- bin/main.dart
+++ bin/main.dart
@@ -1,40 +1,18 @@
 
 
-
 //import 'dart:collection';
 //main(){
-// LinkedHashMap linked = new LinkedHashMap();//ترتب حسب ما أنا مختار وتطبع
-// linked.addAll({'a':'data1'});
-// linked.addAll({'b':'data2'});
-// linked.addAll({'c':'data3'});
-// print(linked);
-//}
-
-//import 'dart:collection';
-//main(){
-//  LinkedHashMap linked = new LinkedHashMap<String,int>();//ترتب حسب ما أنا مختار وتطبع
-//  linked.addAll({'a':1});
-//  linked.addAll({'b':2});
-//  linked.addAll({'c':3});
-//  print(linked); //سيطبع خطأ لأنو القيم لازم تكون dynamic
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  LinkedHashMap linked = new LinkedHashMap<dynamic,dynamic>();//ترتب حسب ما أنا مختار وتطبع
-//  linked.addAll({'a':1});
-//  linked.addAll({'b':2});
-//  linked.addAll({'c':3});
-//  print(linked); //لازم تكون dynamic
+//  SplayTreeMap splaytreemap = new SplayTreeMap(); //نفس الماب ولكنه أسرع ومرن ويمكن تخزين قيم متعددة الأشكال
+//  splaytreemap.addAll({'foo':'dart1'});
+//  splaytreemap.addAll({'bar':'dart2'});
+//  print(splaytreemap);
 //}
 
 
 import 'dart:collection';
 main(){
-  LinkedHashMap linked = new LinkedHashMap<dynamic,dynamic>();//ترتب حسب ما أنا مختار وتطبع
-  linked.addAll({'a':1});
-  linked.addAll({'b':2});
-  linked.addAll({'c':3});
-  print(linked.runtimeType);
+  SplayTreeMap splaytreemap = new SplayTreeMap<String,int>();
+  splaytreemap.addAll({'foo':'dart1'});
+  splaytreemap.addAll({'bar':'dart2'});
+  print(splaytreemap);
 }
