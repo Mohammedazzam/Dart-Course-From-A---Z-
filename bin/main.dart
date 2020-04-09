833a29dacae0398dcbfa3f7a094d26fa0f7bbfd5
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 15:26:50 +0300

    75(Finally error message And how to Implement Exception Class)

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,60 @@
 
-import 'package:test/test.dart';
+//main(){
+//  String name;
+//
+//  try{
+//    print(name.isNotEmpty);
+//  }catch(error){
+//    print(error);
+//  }
+//}
 
 //main(){
 //  String name;
 //
 //  try{
-//    print(name.isEmpty)//بمعنى القيمة خالية;
+//    print(name.isNotEmpty);
 //  }catch(error){
-//    return throw new Exception('The Name Not Have Value');
+//    print(error);
+//  }finally{ //هذه بمعنى اطبع جملة اخيرة عشان أعرف أنا وين
+//    print('End Run Code In Main.dart');
 //  }
 //}
 
 
-
+//سيطبع قبل عملية الاكسيبشن
 //main(){
-//  String name = 'foo';
+//  String name;
 //
 //  try{
-//    print(name.isNotEmpty); //بمعنى المتغير قيمته غير خالية
+//    print(name.isNotEmpty);
 //  }catch(error){
-//    return throw new Exception('The Name Not Have Value');
+//    return throw new DogException().getError();
+//  }finally{ //هذه بمعنى اطبع جملة اخيرة عشان أعرف أنا وين
+//    print('End Run Code In Main.dart');
+//  }
+//}
+//class DogException implements Exception{
+//  String getError(){
+//    return 'Hey I am Error Message';
 //  }
 //}
 
 
-
+//طريقة اخرى للاكسيبشن
 main(){
   String name;
 
   try{
-    print(name.isNotEmpty); //بمعنى المتغير قيمته غير خالية
+    print(name.isNotEmpty);
   }catch(error){
-    return throw new FormatException('The Name Not Have Value');
+    return throw new DogException().getError(error);
+  }finally{ //هذه بمعنى اطبع جملة اخيرة عشان أعرف أنا وين
+    print('This Code In Main.dart');
+  }
+}
+class DogException implements Exception{
+  String getError(val){
+    return val;
   }
 }
