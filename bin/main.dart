3431f53698e864c2ae73110f1254848ae8bfddc5
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-09 16:25:26 +0300

    79(dart collection LinkedHashMap)

--- bin/main.dart
+++ bin/main.dart
@@ -1,162 +1,40 @@
 
+
+
 //import 'dart:collection';
 //main(){
-//  Map mapdata = new Map<String,dynamic>(); //بمعنى ان القيمة التي عينتها لاااااازم تكون من نوع String
-//  mapdata[0] = 'testData';//هيك راح يطبع خطأ لأنو الداتا من نوع int
-//  print(mapdata);
+// LinkedHashMap linked = new LinkedHashMap();//ترتب حسب ما أنا مختار وتطبع
+// linked.addAll({'a':'data1'});
+// linked.addAll({'b':'data2'});
+// linked.addAll({'c':'data3'});
+// print(linked);
+//}
+
+//import 'dart:collection';
+//main(){
+//  LinkedHashMap linked = new LinkedHashMap<String,int>();//ترتب حسب ما أنا مختار وتطبع
+//  linked.addAll({'a':1});
+//  linked.addAll({'b':2});
+//  linked.addAll({'c':3});
+//  print(linked); //سيطبع خطأ لأنو القيم لازم تكون dynamic
 //}
 
 
 //import 'dart:collection';
 //main(){
-//  Map mapdata = new Map<String,dynamic>(); //بمعنى ان القيمة التي عينتها لاااااازم تكون من نوع String
-//  mapdata['data'] = 'testData';
-//  print(mapdata);
-//}
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<int,dynamic>(); //بمعنى ان القيمة التي عينتها لاااااازم تكون من نوع int
-//  mapdata[1] = 'testData';
-//  print(mapdata);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>(); //هيك راح ياخد أيي قيمة
-//  mapdata['100'] = true;
-//  print(mapdata);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//}
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<bool,dynamic>();
-//  mapdata['xyz'] = 'test';
-//  print(mapdata);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<int>();
-//  listData.add(1);
-//  print(listData);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<String>();
-//  listData.add(1);
-//  print(listData);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<String>();
-//  listData.add('foo');
-//  print(listData);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<String>();
-//  listData.add('foo');
-//  print(listData);
-//
-//  HashMap hashdata = new HashMap<String,dynamic>();
-//  hashdata['Test'] = 'dog';
-//  print(hashdata);
-//}
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<String>();
-//  listData.add('foo');
-//  print(listData);
-//
-//  HashMap hashdata = new HashMap<int,dynamic>();
-//  hashdata['Test'] = 'dog';
-//  print(hashdata);
-//}
-
-
-
-//import 'dart:collection';
-//main(){
-//  Map mapdata = new Map<dynamic,dynamic>();
-//  mapdata['xyz'] = true;
-//  print(mapdata);
-//
-//  List listData = new List<String>();
-//  listData.add('foo');
-//  print(listData);
-//
-//  HashMap hashdata = new HashMap<int,dynamic>();
-//  hashdata[1] = 'dog';
-//  print(hashdata);
-//
-//  Set setData = new Set<String>();
-//  setData.add('Test');
-//  print(setData);
+//  LinkedHashMap linked = new LinkedHashMap<dynamic,dynamic>();//ترتب حسب ما أنا مختار وتطبع
+//  linked.addAll({'a':1});
+//  linked.addAll({'b':2});
+//  linked.addAll({'c':3});
+//  print(linked); //لازم تكون dynamic
 //}
 
 
 import 'dart:collection';
 main(){
-  Map mapdata = new Map<dynamic,dynamic>();
-  mapdata['xyz'] = true;
-  print(mapdata);
-
-  List listData = new List<String>();
-  listData.add('foo');
-  print(listData);
-
-  HashMap hashdata = new HashMap<int,dynamic>();
-  hashdata[1] = 'dog';
-  print(hashdata);
-
-  Set setData = new Set<int>();
-  setData.add('Test');
-  print(setData);
+  LinkedHashMap linked = new LinkedHashMap<dynamic,dynamic>();//ترتب حسب ما أنا مختار وتطبع
+  linked.addAll({'a':1});
+  linked.addAll({'b':2});
+  linked.addAll({'c':3});
+  print(linked.runtimeType);
 }
