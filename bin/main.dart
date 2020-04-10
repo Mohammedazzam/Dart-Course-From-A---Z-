8cc20592c521453008c1738a33bb3c56c09ccd95
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-10 19:25:00 +0300

    81(Factory - how it works in dart)

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,123 @@
 
 
-//import 'dart:collection';
 //main(){
-//  SplayTreeMap splaytreemap = new SplayTreeMap(); //نفس الماب ولكنه أسرع ومرن ويمكن تخزين قيم متعددة الأشكال
-//  splaytreemap.addAll({'foo':'dart1'});
-//  splaytreemap.addAll({'bar':'dart2'});
-//  print(splaytreemap);
+//  Dog dog = new Dog('Rex','2');
+//  print(dog.name)
+//;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  Dog(this.name, this.age);
 //}
 
 
-import 'dart:collection';
+//main(){
+//  Dog dog = new Dog();
+//  print(dog.name)
+//  ;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  Dog._(this.name, this.age);
+//}
+
+
+//main(){
+//
+//  var name = "Hello_World";
+//  var result = name.split('_');
+//  print(result);
+//
+//}
+
+
+//main(){
+//
+//
+//  Dog dog = new Dog('Rex 21');
+//  print(dog.age)
+//      ;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  factory Dog(String name){
+//    var doginfo = name.split(' ');
+//    return Dog._(doginfo[0], doginfo[1]);
+//  }
+//  Dog._(this.name, this.age);
+//}
+
+
+//main(){
+//
+//
+//  Dog dog = new Dog('Rex 21');
+//  print(dog.name)
+//  ;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  factory Dog(String name){
+//    var doginfo = name.split(' ');
+//    return Dog._(doginfo[0], doginfo[1]);
+//  }
+//  Dog._(this.name, this.age);
+//}
+
+
+//main(){
+//
+//
+//  Dog dog = new Dog('Rex 21');
+//  print(dog.name)
+//  ;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  factory Dog(String name){
+//    var doginfo = name.split(' ');
+//    return Dog._intern(doginfo[0], doginfo[1]);
+//  }
+//  Dog._intern(this.name, this.age); //طريقة اخرى
+//}
+
+
+//main(){
+//
+//
+//  Dog dog = new Dog('Rex 21');
+//  print(dog.name)
+//  ;
+//}
+//class Dog{
+//  final String name;
+//  final String age;
+//  factory Dog(String name){
+//    var doginfo = name.split(' ');
+//    return Dog._data(doginfo[0], doginfo[1]);
+//  }
+//  Dog._data(this.name, this.age); //طريقة اخرى
+//}
+
+
 main(){
-  SplayTreeMap splaytreemap = new SplayTreeMap<String,int>();
-  splaytreemap.addAll({'foo':'dart1'});
-  splaytreemap.addAll({'bar':'dart2'});
-  print(splaytreemap);
+
+
+  Dog dog = new Dog('Rex 21');
+  print(dog.name)
+  ;
+}
+class Dog{
+  final String name;
+  final String age;
+  factory Dog(String name){
+    var doginfo = name.split(' ');
+    return Dog._data(doginfo[0], doginfo[1]);
+  }
+  Dog._data(this.name, this.age); //طريقة اخرى
 }
