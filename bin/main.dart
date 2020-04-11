98bc37c28f49aef6b184a99896396e19f2d2883e
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-11 18:23:27 +0300

    86(for in loop)

--- bin/main.dart
+++ bin/main.dart
@@ -1,73 +1,19 @@
-//import 'dart:io';
-//import 'dart:async';
-//import 'dart:isolate';
-//
-//Isolate isolate; //عملت هيك  متغير من نوع  isolate
-//void begin()async{
-//  ReceivePort receivePort = ReceivePort();
-//  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
-//  receivePort.listen((data){
-//    stdout.write(data);
-//  });
-//}
-//void runbegin(SendPort sendport){
-//  String message = 'Hello';
-//  stdout.write(message);
-//  sendport.send(message);
-//
-//
-//}
-//main() async{
-//  await begin();
-//
+
+//main() {
+//  List data = ['foo','bar','xyz'];
+//  for(var result in data){
+//    print(result);
+//  };
 //}
 
-
-//import 'dart:io';
-//import 'dart:async';
-//import 'dart:isolate';
-//
-//Isolate isolate; //عملت هيك  متغير من نوع  isolate
-//void begin()async{
-//  ReceivePort receivePort = ReceivePort();
-//  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
-//  receivePort.listen((data){
-//    stdout.write(data);
-//  });
-//}
-//void runbegin(SendPort sendport){
-//  String message = 'Hello';
-////  stdout.write(message);
-//  sendport.send(message);
-//
-//
-//}
-//main() async{
-//  await begin();
-//
-//}
-
-
-import 'dart:io';
-import 'dart:async';
-import 'dart:isolate';
-
-Isolate isolate; //عملت هيك  متغير من نوع  isolate
-void begin()async{
-  ReceivePort receivePort = ReceivePort();
-  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
-  receivePort.listen((data){
-    stdout.write(data);
-  });
-}
-void runbegin(SendPort sendport){
-  String message = 'Hello';
-//  stdout.write(message);
-  sendport.send(message);
-
-
-}
-main() async{
-  await begin();
-
+//الفرق بين الطريقتين
+main() {
+  List data = ['foo','bar','xyz'];
+  for(int index = 0; index < data.length;index++){
+    print(data[index]);
+  }
+  print('----------');
+    for(var result in data){ //هيك أنا بسند قيم داخل ال result
+    print(result);
+  };
 }
