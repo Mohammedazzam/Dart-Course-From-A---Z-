61574d92e792adb9e7431ced645d887906970475
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-11 16:26:25 +0300

    83(what is the Stream ! & how it)

--- bin/main.dart
+++ bin/main.dart
@@ -1,90 +1,47 @@
-//import 'dart:io';
+//import 'dart:async';
 //main() async{
-//  var textfile = File('data.txt');
-//  var filedata;
 //
-//  print(await textfile.exists()); //لو عملت run راح يطبعلي true بمعنى أن الملف موجود
-//  if(await textfile.exists()){
-//
-//  }
+//  var streamController = StreamController();
+//  streamController.stream.listen((text){
+//    print(text);
+//  });
 //}
 
 
-//import 'dart:io';
+//import 'dart:async';
 //main() async{
-//  var textfile = File('data.txt');
-//  var filedata;
 //
-//  if(await textfile.exists()){
-//      filedata = await textfile.readAsString();
-//      print(filedata); //سيطب عالكلام إلي في ملف ال datr
-//  }
+//  var streamController = StreamController();
+//  streamController.stream.listen((text){
+//    print(text);
+//  });
+//  streamController.sink.add('Hello Datr I am Hear');// لو عملت run سيطبعها
 //}
 
 
-//لو بدي اعمل copy لهذا الملف بكل سهولة
-//import 'dart:io';
+//import 'dart:async';
 //main() async{
-//  var textfile = File('data.txt');
-//  var filedata;
 //
-//  if(await textfile.exists()){
-//    filedata = await textfile.readAsString();
-//    
-//    var copy= await File('data2.txt').writeAsString(filedata);
-//    //لو عملت run سينشئ ملف جديد وسيضع نفس ال data الموجودة في الملف السابق
-//  }
+//  var streamController = StreamController();
+//  streamController.stream.listen((text){
+//    print(text);
+//  });
+//  streamController.sink.add('Hello Datr I am Hear');
+//  print('welcome in Dart'); //سيطبع هذه في الأول ثم الأولى
+//
 //}
 
 
-//import 'dart:io';
+//import 'dart:async';
 //main() async{
-//  var textfile = File('data.txt');
-//  var filedata;
 //
-//  if(await textfile.exists()){
-//    filedata = await textfile.readAsString();
+//  var streamController = StreamController();
+//  streamController.stream.listen((text){
+//    print(text);
+//  });
+//  streamController.sink.add('Hello Datr I am Hear');
+//  streamController.sink.close();
+//  print('welcome in Dart');
 //
-//    var copy= await File('data2.txt').writeAsString(filedata);
-//
-//    print(await copy.length());//سيطبع طول الملف بعدد الحروف المكتوبة
 //}
-//}
-
-
-//import 'dart:io';
-//main() async{
-//  var textfile = File('data.txt');
-//  var filedata;
-//
-//  if(await textfile.exists()){
-//    filedata = await textfile.readAsString();
-//
-//    var copy= await File('data2.txt').writeAsString(filedata);
-//
-//    print(await copy.length());//سيطبع طول الملف بعدد الحروف المكتوبة
-//    var open = await File('data2.txt').openWrite().write('Hi Dart'); // سيطبع في الملف هذه الجملة وعدد الطول
-//  }
-//}
-
-
-//لطباعة المسار كاملاً
-import 'dart:io';
-import 'package:path/path.dart';
-main() async{
-  var filepath = join(dirname(Platform.script.toFilePath()),'dart.txt'); //سيطبع المسار كامل
-  print(filepath);
-  var textfile = File('data.txt');
-  var filedata;
-
-  if(await textfile.exists()){
-    filedata = await textfile.readAsString();
-
-    var copy= await File('data2.txt').writeAsString(filedata);
-
-    print(await copy.length());//سيطبع طول الملف بعدد الحروف المكتوبة
-    var open = await File('data2.txt').openWrite().write('Hi Dart'); // سيطبع في الملف هذه الجملة وعدد الطول
-  }
-}
-
 
