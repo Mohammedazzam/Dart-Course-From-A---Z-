b78cdf20a50d8406e0148c42d62eff7f313c8dec
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-11 18:16:06 +0300

    84(What is Isolate and how it works )

--- bin/main.dart
+++ bin/main.dart
@@ -1,47 +1,73 @@
+//import 'dart:io';
 //import 'dart:async';
-//main() async{
+//import 'dart:isolate';
 //
-//  var streamController = StreamController();
-//  streamController.stream.listen((text){
-//    print(text);
+//Isolate isolate; //عملت هيك  متغير من نوع  isolate
+//void begin()async{
+//  ReceivePort receivePort = ReceivePort();
+//  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
+//  receivePort.listen((data){
+//    stdout.write(data);
 //  });
 //}
-
-
-//import 'dart:async';
-//main() async{
+//void runbegin(SendPort sendport){
+//  String message = 'Hello';
+//  stdout.write(message);
+//  sendport.send(message);
 //
-//  var streamController = StreamController();
-//  streamController.stream.listen((text){
-//    print(text);
-//  });
-//  streamController.sink.add('Hello Datr I am Hear');// لو عملت run سيطبعها
+//
 //}
-
-
-//import 'dart:async';
 //main() async{
-//
-//  var streamController = StreamController();
-//  streamController.stream.listen((text){
-//    print(text);
-//  });
-//  streamController.sink.add('Hello Datr I am Hear');
-//  print('welcome in Dart'); //سيطبع هذه في الأول ثم الأولى
+//  await begin();
 //
 //}
 
 
+//import 'dart:io';
 //import 'dart:async';
-//main() async{
+//import 'dart:isolate';
 //
-//  var streamController = StreamController();
-//  streamController.stream.listen((text){
-//    print(text);
+//Isolate isolate; //عملت هيك  متغير من نوع  isolate
+//void begin()async{
+//  ReceivePort receivePort = ReceivePort();
+//  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
+//  receivePort.listen((data){
+//    stdout.write(data);
 //  });
-//  streamController.sink.add('Hello Datr I am Hear');
-//  streamController.sink.close();
-//  print('welcome in Dart');
+//}
+//void runbegin(SendPort sendport){
+//  String message = 'Hello';
+////  stdout.write(message);
+//  sendport.send(message);
+//
+//
+//}
+//main() async{
+//  await begin();
 //
 //}
 
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:isolate';
+
+Isolate isolate; //عملت هيك  متغير من نوع  isolate
+void begin()async{
+  ReceivePort receivePort = ReceivePort();
+  isolate = await Isolate.spawn(runbegin, receivePort.sendPort); //isolate عبارة عن فاريابل
+  receivePort.listen((data){
+    stdout.write(data);
+  });
+}
+void runbegin(SendPort sendport){
+  String message = 'Hello';
+//  stdout.write(message);
+  sendport.send(message);
+
+
+}
+main() async{
+  await begin();
+
+}
